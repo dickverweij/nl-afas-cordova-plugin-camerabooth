07fefd6e6eac8861438794088ab648e3e1744875
afasdev <d.verweij@afas.nl>
2016-03-17 10:53:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+Camera booth plugin
+==========================
+Camera booth plugin, shows preview and after counting down takes a picture.
+
+Requirements
+-------------
+- iOS 8 or higher
+- Cordova 3.0 or higher
+
+    Installation
+-------------
+    cordova plugin add nl-afas-cordova-plugin-camerabooth
+
+Usage
+------
+    // n = number of pictures taken
+	// images is array of base64 image data
+
+    cordova.plugins.CameraBooth.start(n).then(function (images){});
+
+LICENSE
+--------
+The MIT License (MIT)
+
+Copyright (c) 2016 dickydick1969@hotmail.com Dick Verweij AFAS Software BV - d.verweij@afas.nl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/ios/CameraBoothViewController.m
@@ -0,0 +1,148 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Dick Verweij dickydick1969@hotmail.com, d.verweij@afas.nl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#import "CameraBoothViewController.h"
+@import AVFoundation;
+@import QuartzCore;
+@import ImageIO;
+
+@implementation CameraBoothViewController
+
+@synthesize label,cameraView,imageView,completion;
+
+- (AVCaptureDevice *)frontCamera {
+    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *device in devices) {
+        if ([device position] == AVCaptureDevicePositionFront) {
+            return device;
+        }
+    }
+    return nil;
+}
+
+- (void)takePicture:(AVCaptureStillImageOutput *)stillImageOutput {
+    
+    AVCaptureConnection *videoConnection = nil;
+    for (AVCaptureConnection *connection in stillImageOutput.connections)
+    {
+        for (AVCaptureInputPort *port in [connection inputPorts])
+        {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo] )
+            {
+                videoConnection = connection;
+                break;
+            }
+        }
+        if (videoConnection) { break; }
+    }
+    
+    
+    [stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error)
+     {
+         CFDictionaryRef exifAttachments = CMGetAttachment( imageSampleBuffer, kCGImagePropertyExifDictionary, NULL);
+         if (exifAttachments)
+         {
+             // Do something with the attachments.
+             NSLog(@"attachements: %@", exifAttachments);
+         }
+         else
+             NSLog(@"no attachments");
+         
+         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
+         [self completion] ([imageData base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength]);
+     }];
+}
+
+- (void)countDown: (NSString *) labelText completion: (void (^)()) completionCall
+{
+    label.alpha = 0;
+    label.transform = CGAffineTransformMakeScale(0.2, 0.2);
+    label.text = labelText;
+    
+    [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         label.alpha = 1;
+                         label.transform = CGAffineTransformMakeScale(1.0, 1.0);}
+                     completion:^(BOOL finished){
+                         [UIView animateWithDuration:0.5 delay:1 options:UIViewAnimationOptionCurveEaseIn
+                                          animations:^{
+                                              label.alpha = 0;}
+                          completion:^(BOOL finished) {
+                              completionCall();
+                          }];
+                     
+                     }];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    
+ 
+    
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    session.sessionPreset = AVCaptureSessionPresetMedium;
+    
+    CALayer *viewLayer = self.cameraView.layer;
+    NSLog(@"viewLayer = %@", viewLayer);
+    
+    AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    
+    captureVideoPreviewLayer.frame = self.cameraView.bounds;
+    [self.cameraView.layer addSublayer:captureVideoPreviewLayer];
+    
+    AVCaptureDevice *device = [self frontCamera];
+    
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    if (!input) {
+        // Handle the error appropriately.
+        NSLog(@"ERROR: trying to open camera: %@", error);
+    }
+    [session addInput:input];
+    AVCaptureStillImageOutput * stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [stillImageOutput setOutputSettings:outputSettings];
+    
+    [session addOutput:stillImageOutput];
+    
+    [session startRunning];
+    
+    [self countDown: @"3" completion:^() {
+        [self countDown: @"2" completion:^() {
+            [self countDown: @"1" completion:^() {
+                [self takePicture:stillImageOutput];
+            }];
+        }];
+    }];
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.    
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
